39849b2bf7597c15a421d88581d5110686b9ea50
Nerx2008 <nikitahysnutdinov@gmail.com>
2024-02-07 11:40:05 +0400

    Фикс разрешения изображений

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   scrollToTopBtn.addEventListener('click', function() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   });
 });
